34d85b77bdba611af5562ddbdc60a8f5b397b24e
lucas arcanjo <lucas.saolliveira@gmail.com>
2023-08-11 19:47:39 -0300

    more details

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
-
 @NgModule({
   declarations: [
     AppComponent
